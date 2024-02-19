700b11814c99eaaa124e044779d81a0f3892f3d7
Trevor Hubbard <trevormhubbard@gmail.com>
2024-02-18 20:33:33 -0500

    Changing the UI to be viewable on port 80

--- ui/deployment/deploy_ui.sh
+++ ui/deployment/deploy_ui.sh
@@ -17,4 +17,4 @@ sudo docker rm neural-needledrop-ui-container || true
 
 # Run the UI container
 echo "Running the UI container..."
-sudo docker run -p 8080:8080 --name neural-needledrop-ui-container -d us-central1-docker.pkg.dev/neural-needledrop/neural-needledrop-webapp/neural-needledrop-ui:latest
+sudo docker run -p 80:8080 --name neural-needledrop-ui-container -d us-central1-docker.pkg.dev/neural-needledrop/neural-needledrop-webapp/neural-needledrop-ui:latest
